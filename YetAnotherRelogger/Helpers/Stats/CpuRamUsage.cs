d767c22c2d8bd1950f8617fbef03d3fd7bf35849
Jorrit <sinterlkaas@gmail.com>
2012-12-18 14:34:14 +0100

    Added DB/D3 Cpu/Ram stats

---
+++ YetAnotherRelogger/Helpers/Stats/CpuRamUsage.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using ComTypes = System.Runtime.InteropServices.ComTypes;
+
+namespace YetAnotherRelogger.Helpers.Stats
+{
+    public class CpuRamUsage
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool GetSystemTimes(out ComTypes.FILETIME lpIdleTime, out ComTypes.FILETIME lpKernelTime, out ComTypes.FILETIME lpUserTime);
+
+        private ComTypes.FILETIME _lastSysKernel;
+        private ComTypes.FILETIME _lastSysUser;
+        private HashSet<ProcUsage> _procUsageList;
+
+        public CpuRamUsage()
+        {
+            _lastSysKernel.dwHighDateTime = _lastSysKernel.dwLowDateTime = 0;
+            _lastSysUser.dwHighDateTime = _lastSysUser.dwLowDateTime = 0;
+            _procUsageList = new HashSet<ProcUsage>();
+        }
+
+        public bool Update()
+        {
+            ComTypes.FILETIME sysIdle, sysKernel, sysUser;
+
+            // Check if we can get current system cpu times
+            if (!GetSystemTimes(out sysIdle, out sysKernel, out sysUser))
+                return false;
+
+            // Calculate tot system cpu time
+            var sysKernelDiff = SubtractTimes(sysKernel, _lastSysKernel);
+            var sysUserDiff = SubtractTimes(sysUser, _lastSysUser);
+            var sysTotal = sysKernelDiff + sysUserDiff;
+
+            var newList = new HashSet<ProcUsage>();
+            foreach (var proc in Process.GetProcesses())
+            {
+                try
+                {
+                    // Skip proc with id 0
+                    if (proc.Id == 0) continue;
+
+                    Int64 procTotal;
+                    var p = GetById(proc.Id);
+                    if (p != null)
+                        procTotal = proc.TotalProcessorTime.Ticks - p.LastProcTime.Ticks;
+                    else
+                        procTotal = 0;
+
+                    // Add Process to list
+                    newList.Add(new ProcUsage
+                    {
+                        Process = proc,
+                        Usage = new Usage
+                        {
+                            Cpu = ((100.0 * procTotal) / sysTotal), // Calculate process CPU Usage
+                            Memory = proc.PrivateMemorySize64
+                        },
+                        LastProcTime = proc.TotalProcessorTime
+                    });
+                }
+                catch
+                {
+                    continue;
+                }
+                Thread.Sleep(1); // be nice for cpu
+            }
+
+            // Update last system times
+            _lastSysKernel = sysKernel;
+            _lastSysUser = sysUser;
+
+            // Update Process list
+            _procUsageList = newList;
+            return true;
+        }
+
+        public ProcUsage GetById(int id)
+        {
+            try
+            {
+                return _procUsageList.FirstOrDefault(x => x.Process.Id == id);
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        /// <summary>
+        /// Get Process CPU Usage
+        /// </summary>
+        /// <param name="id">Process Id</param>
+        /// <returns>Cpu usage</returns>
+        public Usage GetUsageById(int id)
+        {
+            var p = GetById(id);
+            return p != null ? p.Usage : null;
+        }
+
+        private static UInt64 SubtractTimes(ComTypes.FILETIME a, ComTypes.FILETIME b)
+        {
+            var aInt = ((UInt64)(a.dwHighDateTime << 32)) | (UInt64)a.dwLowDateTime;
+            var bInt = ((UInt64)(b.dwHighDateTime << 32)) | (UInt64)b.dwLowDateTime;
+            return aInt - bInt;
+        }
+
+        public class ProcUsage
+        {
+            public ProcUsage()
+            {
+                LastProcTime = TimeSpan.MinValue;
+                Process = new Process();
+            }
+            public TimeSpan LastProcTime;
+            public Process Process;
+            public Usage Usage;
+        }
+
+        public class Usage
+        {
+            public double Cpu;
+            public long Memory;
+        }
+    }
+}
--- YetAnotherRelogger/StatsUpdater.cs
+++ YetAnotherRelogger/StatsUpdater.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 using YetAnotherRelogger.Helpers;
+using YetAnotherRelogger.Helpers.Stats;
 
 namespace YetAnotherRelogger
 {
@@ -32,7 +34,6 @@ namespace YetAnotherRelogger
             _statsUpdater.Start();
         }
 
-
         public void Stop()
         {
             _statsUpdater.Abort();
@@ -40,14 +41,78 @@ namespace YetAnotherRelogger
 
         public void StatsUpdaterWorker()
         {
+            var usages = new CpuRamUsage();
+            var cpuUsage = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            var TotalRam = new PerformanceCounter("Memory", "Available MBytes");
             while (true)
             {
+                // Update Cpu/Ram Usage
+                usages.Update();
+
+                double diabloCpuUsage = 0;
+                long diabloRamUsage = 0;
+                int diabloCount = 0;
+                double demonbuddyCpuUsage = 0;
+                long demonbuddyRamUsage = 0;
+                int demonbuddyCount = 0;
+                
                 foreach (var bot in BotSettings.Instance.Bots)
                 {
                     // Update bot uptime
                     bot.RunningTime = bot.IsRunning ? DateTime.Now.Subtract(bot.StartTime).ToString(@"hh\hmm\mss\s") : "";
+
+
+                    // Calculate total Cpu/Ram usage for Diablo
+                    if (bot.Diablo.IsRunning)
+                    {
+                        try
+                        {
+                            var usage = usages.GetUsageById(bot.Diablo.Proc.Id);
+                            diabloCpuUsage += usage.Cpu;
+                            diabloRamUsage += usage.Memory;
+                            diabloCount++;
+                        }
+                        catch
+                        {}
+                    }
+                    // Calculate total Cpu/Ram usage for Demonbuddy
+                    if (bot.Demonbuddy.IsRunning)
+                    {
+                        try
+                        {
+                            var usage = usages.GetUsageById(bot.Demonbuddy.Proc.Id);
+                            demonbuddyCpuUsage += usage.Cpu;
+                            demonbuddyRamUsage += usage.Memory;
+                            demonbuddyCount++;
+                        }
+                        catch
+                        {}
+                    }
+
+                    
                 }
-                Thread.Sleep(1000);
+                // Update Stats label on mainform
+                updateMainformStatsLabel(
+                    string.Format("Cpu Usage : Diablo[{0}] {1}%, Demonbuddy[{2}] {3}%, Total {4}%" + Environment.NewLine + 
+                                  "Ram Usage : Diablo[{0}] {5}Gb, Demonbuddy[{2}] {6}Mb, Total {7}Gb",
+                                  diabloCount, Math.Round(diabloCpuUsage,1), demonbuddyCount, Math.Round(demonbuddyCpuUsage,1), Math.Round(diabloCpuUsage+demonbuddyCpuUsage,1),
+                                  Math.Round(diabloRamUsage / Math.Pow(2 , 30), 2), Math.Round(demonbuddyRamUsage / Math.Pow(2 , 20),2), Math.Round((diabloRamUsage / Math.Pow(2,30)) + (demonbuddyRamUsage / Math.Pow(2,30)), 2)));
+                Thread.Sleep(500);
+            }
+        }
+
+        private void updateMainformStatsLabel(string text)
+        {
+            if (Program.Mainform != null && Program.Mainform.labelStats != null)
+            {
+                try
+                {
+                    Program.Mainform.Invoke(new Action(() => Program.Mainform.labelStats.Text = text));
+                }
+                catch
+                {
+                    // Failed! do nothing
+                }
             }
         }
     }
