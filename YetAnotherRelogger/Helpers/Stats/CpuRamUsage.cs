62780984e121b08b27cdeae4217115f2e7a6f751
Jorrit <sinterlkaas@gmail.com>
2013-01-07 15:48:58 +0100

    CPU Graph fixed false readings

--- YetAnotherRelogger/Helpers/Stats/CpuRamUsage.cs
+++ YetAnotherRelogger/Helpers/Stats/CpuRamUsage.cs
@@ -23,80 +23,119 @@ namespace YetAnotherRelogger.Helpers.Stats
         public CpuRamUsage()
         {
             TotalCpuUsage = 0;
-            _lastSysKernel.dwHighDateTime = _lastSysKernel.dwLowDateTime = 0;
-            _lastSysUser.dwHighDateTime = _lastSysUser.dwLowDateTime = 0;
-            _lastSysIdle.dwHighDateTime = _lastSysIdle.dwLowDateTime = 0;
             _procUsageList = new HashSet<ProcUsage>();
         }
 
+        private bool _initialized;
+        private bool Init()
+        {
+            return GetSystemTimes(out _lastSysIdle, out _lastSysKernel, out _lastSysUser);
+        }
+
+        private bool glitchRecover;
         public bool Update()
         {
-            ComTypes.FILETIME sysIdle, sysKernel, sysUser;
-
-            // Check if we can get current system cpu times
-            if (!GetSystemTimes(out sysIdle, out sysKernel, out sysUser))
-                return false;
-
-            // Calculate tot system cpu time
-            var sysKernelDiff = SubtractTimes(sysKernel, _lastSysKernel);
-            var sysUserDiff = SubtractTimes(sysUser, _lastSysUser);
-            var sysIdleDiff = SubtractTimes(sysIdle, _lastSysIdle);
-            var sysTotal = sysKernelDiff + sysUserDiff;
-            // Calculate total Cpu usage
-            var totalUsage = sysTotal > 0 ? 100 - ((100d * sysIdleDiff) / sysTotal) : TotalCpuUsage;
-            TotalCpuUsage = totalUsage < 0 ? 0 : totalUsage;
-
-            var newList = new HashSet<ProcUsage>();
-            foreach (var proc in Process.GetProcesses())
+            try
             {
-                try
+                ComTypes.FILETIME sysIdle, sysKernel, sysUser;
+
+                if (!_initialized)
                 {
-                    // Skip proc with id 0
-                    if (proc.Id == 0) continue;
+                    _initialized = Init();
+                    return _initialized;
+                }
 
-                    Int64 procTotal;
-                    double usage;
-                    var p = GetById(proc.Id);
-                    if (p != null)
-                    {
-                        procTotal = proc.TotalProcessorTime.Ticks - p.LastProcTime.Ticks;
-                        usage = sysTotal > 0 ?((100.0*procTotal)/sysTotal) : p.Usage.Cpu;
-                    }
-                    else
-                    {
-                        procTotal = 0;
-                        usage = sysTotal > 0 ?((100.0*procTotal)/sysTotal) : 0;
-                    }
+                // Check if we can get current system cpu times
+                if (!GetSystemTimes(out sysIdle, out sysKernel, out sysUser))
+                    return false;
 
-                    // Add Process to list
-                    newList.Add(new ProcUsage
+                // Calculate tot system cpu time
+                var sysKernelDiff = SubtractTimes(sysKernel, _lastSysKernel);
+                var sysUserDiff = SubtractTimes(sysUser, _lastSysUser);
+                var sysIdleDiff = SubtractTimes(sysIdle, _lastSysIdle);
+                var sysTotal = sysKernelDiff + sysUserDiff;
+
+                if (!validDiff((long)sysKernelDiff) || !validDiff((long)sysUserDiff) || !validDiff((long)sysIdleDiff))
+                {
+                    Debug.WriteLine("Stats: Negative Tick Difference");
+                    Debug.WriteLine("kernel: {0,-20} :: {1,-20} Diff:{2,-20} :: {3} miliseconds", ((UInt64)(sysKernel.dwHighDateTime << 32)) | (UInt64)sysKernel.dwLowDateTime, ((UInt64)(_lastSysKernel.dwHighDateTime << 32)) | (UInt64)_lastSysKernel.dwLowDateTime, sysKernelDiff, TimeSpan.FromTicks((long)sysKernelDiff).TotalMilliseconds);
+                    Debug.WriteLine("user  : {0,-20} :: {1,-20} Diff:{2,-20} :: {3} miliseconds", ((UInt64)(sysUser.dwHighDateTime << 32)) | (UInt64)sysUser.dwLowDateTime, ((UInt64)(_lastSysUser.dwHighDateTime << 32)) | (UInt64)_lastSysUser.dwLowDateTime, sysUserDiff, TimeSpan.FromTicks((long)sysUserDiff).TotalMilliseconds);
+                    Debug.WriteLine("idle  : {0,-20} :: {1,-20} Diff:{2,-20} :: {3} miliseconds", ((UInt64)(sysIdle.dwHighDateTime << 32)) | (UInt64)sysIdle.dwLowDateTime, ((UInt64)(_lastSysIdle.dwHighDateTime << 32)) | (UInt64)_lastSysIdle.dwLowDateTime, sysIdleDiff, TimeSpan.FromTicks((long)sysIdleDiff).TotalMilliseconds);
+
+                    glitchRecover = true; // mark to recover from glitch
+                    _lastSysKernel = sysKernel;
+                    _lastSysUser = sysUser;
+                    _lastSysIdle = sysIdle;
+                    Thread.Sleep(100);// give windows time to recover
+                    return Update();
+                }
+
+                // Calculate total Cpu usage
+                var totalUsage = sysTotal > 0 ? ((sysTotal - sysIdleDiff) * 100d / sysTotal) : TotalCpuUsage;
+                TotalCpuUsage = totalUsage < 0 ? TotalCpuUsage : totalUsage;
+
+                var newList = new HashSet<ProcUsage>();
+                foreach (var proc in Process.GetProcesses())
+                {
+                    try
                     {
-                        Process = proc,
-                        Usage = new Usage
+                        // Skip proc with id 0
+                        if (proc.Id == 0) continue;
+
+                        Int64 procTotal;
+                        var oldCpuUsage = 0d;
+                        var p = GetById(proc.Id);
+                        if (p != null)
                         {
-                            Cpu = usage, // Calculate process CPU Usage
-                            Memory = proc.PrivateMemorySize64
-                        },
-                        LastProcTime = proc.TotalProcessorTime
-                    });
+                            procTotal = proc.TotalProcessorTime.Ticks - p.LastProcTime.Ticks;
+                            oldCpuUsage = p.Usage.Cpu;
+                        }
+                        else
+                            procTotal = 0;
+
+                        var usage = glitchRecover ? oldCpuUsage : ((100.0 * procTotal) / sysTotal); // Calculate process CPU Usage
+                        // Add Process to list
+                        newList.Add(new ProcUsage
+                        {
+                            Process = proc,
+                            Usage = new Usage
+                            {
+                                Cpu = usage,
+                                Memory = proc.PrivateMemorySize64
+                            },
+                            LastProcTime = proc.TotalProcessorTime
+                        });
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                    Thread.Sleep(1); // be nice for cpu
                 }
-                catch
-                {
-                    continue;
-                }
-                Thread.Sleep(1); // be nice for cpu
+
+                // Update last system times
+                _lastSysKernel = sysKernel;
+                _lastSysUser = sysUser;
+                _lastSysIdle = sysIdle;
+
+                // unmark glitch recover
+                glitchRecover = false;
+
+                // Update Process list
+                _procUsageList = newList;
             }
-
-            // Update last system times
-            _lastSysKernel = sysKernel;
-            _lastSysUser = sysUser;
-            _lastSysIdle = sysIdle;
-
-            // Update Process list
-            _procUsageList = newList;
+            catch (Exception ex)
+            {
+                Logger.Instance.WriteGlobal(ex.ToString());
+                return false;
+            }
             return true;
         }
 
+        private bool validDiff(long ticks)
+        {
+            return TimeSpan.FromTicks(ticks).TotalMilliseconds > 0;
+        }
         public ProcUsage GetById(int id)
         {
             try
